5632bd9f0b20fded90de5eda5c3ced6dd7ec2423
skye2026s <skye25@vt.edu>
2026-09-17 14:26:42 -0400

    Adv Class start
    The to be implemented adventure class

---
+++ Adventures of The Pilgrim/src/AOTP/Adventure.java
@@ -0,0 +1,17 @@
+package AOTP;
+import student.adventure.Game;
+/*
+ * // -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author asce
+ *  @version Sep 17, 2026
+ */
+@SuppressWarnings("javadoc")
+public class Adventure
+{
+   // Time to build
+}
