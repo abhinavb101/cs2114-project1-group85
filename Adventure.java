818d8700de50f28d111d030344a782e5794e423a
skye2026s <skye25@vt.edu>
2026-09-17 16:18:34 -0400

    Add files via upload

---
+++ Adventure.java
@@ -0,0 +1,37 @@
+package AOTP;
+import student.adventure.Game;
+/*
+ * // -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it
+ * 
+ *  @author asce
+ *  @version Sep 17, 2026
+ */
+@SuppressWarnings("javadoc")
+public class Adventure
+{
+   // Should include all methods from UML
+    
+    
+    public String welcomMessage()
+    {
+        return "Hello";
+    }
+    
+ public void createCommands()
+    {
+        
+    }
+    
+    public void createRooms()
+    {
+        
+    }
+    
+
+}
+
+    
