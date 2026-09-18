620b2e20b1103727c4f30512a8df364c9768a6e2
bryanv072026S <bryanvicente960@gmail.com>
2026-09-18 17:00:11 -0400

    Maybe I did it
    We might be cooked

--- Final Adventures of The Pilgrim/src/finalCode/Adventure.java
+++ Final Adventures of The Pilgrim/src/finalCode/Adventure.java
@@ -1,10 +1,44 @@
 package finalCode;
-public class Adventure
+import student.adventure.*;
+public class Adventure extends Game
 {
-    //~ Fields ................................................................
+    public Adventure() {
+        super(new Pilgrim(), new Parser());
+    }
 
-    //~ Constructors ..........................................................
+    @Override
+    public String welcomeMessage()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public void createCommands()
+    {
+        // TODO Auto-generated method stub
+        
+    }
 
-    //~Public  Methods ........................................................
-
+    @Override
+    public void createRooms()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public Pilgrim getPilgrim()
+    {
+        return null;
+    }
+    
+    public void placeItems()
+    {
+        
+    }
+    
+    public void connectRooms()
+    {
+        
+    }
 }
