d6dc8856eb8ab83d6f659503de80124dfd0e18ba
bryanv072026S <bryanvicente960@gmail.com>
2026-09-23 19:23:38 -0400

    Big Changes (for real)

--- finalCode/MovementCommand.java
+++ finalCode/MovementCommand.java
@@ -1,10 +1,32 @@
 package finalCode;
-public class MovementCommand
+import student.adventure.*;
+public class MovementCommand extends Command
 {
     //~ Fields ................................................................
-
+    private String direction;
     //~ Constructors ..........................................................
-
+    public MovementCommand(String newDirection)
+    {
+        direction = newDirection;
+    }
     //~Public  Methods ........................................................
-
+    public boolean execute(Player newPlayer)
+    {
+        Pilgrim guy = (Pilgrim) newPlayer;
+        if (direction == null)
+        {
+            System.out.println("Move where?");
+            return false;
+        }
+        Room curr = guy.getCurrentRoom();
+        Room next = curr.getExit(direction);
+        if (next == null)
+        {
+            System.out.println("You will get lost if you go " + direction + ".");
+            return false;
+        }
+        guy.setCurrentRoom(next);
+        System.out.println(guy.getCurrentRoom().getLongDescription());
+        return false;
+    }
 }
