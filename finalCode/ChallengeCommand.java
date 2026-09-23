812831a842563e1875ec81f347cf672646f738d2
skye2026s <skye25@vt.edu>
2026-09-23 14:26:09 -0400

    UseCommand & Combat system
    Implemented Combat system and creation of empty UseCommand

--- finalCode/ChallengeCommand.java
+++ finalCode/ChallengeCommand.java
@@ -1,24 +1,44 @@
 package finalCode;
 import student.adventure.*;
 import java.util.*;
+import java.util.Scanner;
 
-public class ChallengeCommand
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Richard
+ *  @version Sep 23, 2026
+ */
+public class ChallengeCommand extends Combat
 {
     private Adventure game;
     private Pilgrim pilgrim;
-    private Enemies enemys;
+    private Enemies enemy;
     
     //~ Fields ................................................................
-    public ChallengeCommand(Adventure game, Pilgrim pilgrim, Enemies enemy) {
-        this.game = game;
-        this.pilgrim = pilgrim;
+    
+    public ChallengeCommand(Pilgrim pilgrim, Enemies enemy, Adventure game) {
+        super(pilgrim, enemy);
         this.enemy = enemy;
     }
     
-    public void execute(String enemyName, Enemies enemy) {
-        while (pilgrim.isAlive() && enemy.isAlive()) {
-            pilgrim.attack(enemy);
+    public void execute(Pilgrim pilgrim, String enemyName) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Begin Combat with: " + enemyName + "?" +
+            "/n [Yes] or [No]");
+        String choice = scan.nextLine();
+        if (choice.equals("Yes")) {
+            super.start();
         }
+        else {
+            return;
+        }
+        
+           // Likely finished Challenge // Sep 23
+        
     }
     
     //~ Constructors ..........................................................
