15f5abc95988c5b7254735df2fdc89331957810d
bryanv072026S <bryanvicente960@gmail.com>
2026-09-23 18:42:46 -0400

    small change

--- finalCode/UnfortunateEvent.java
+++ finalCode/UnfortunateEvent.java
@@ -3,8 +3,8 @@ public class UnfortunateEvent extends Obstacle
 {
     //~ Fields ................................................................
     //~ Constructors ..........................................................
-    public UnfortunateEvent(String name, int damage) {
-        super(name, damage);
+    public UnfortunateEvent(String name, String newDescription,int damage) {
+        super(name, newDescription, damage);
     }
     //~Public  Methods ........................................................
 
